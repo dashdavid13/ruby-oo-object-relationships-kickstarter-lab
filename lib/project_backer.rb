7649768caa8732a4977bd90d51ee05a19296cd82
dashdavid13 <davidamedina619@gmail.com>
2020-10-30 16:31:23 -0400

    done

---
+++ lib/project_backer.rb
@@ -0,0 +1,18 @@
+class ProjectBacker
+    @@all = []
+    
+    def self.all
+        @@all
+    end 
+
+    attr_accessor :project, :backer
+
+    def initialize(project,backer)
+        @project = project
+        @backer = backer
+        @@all << self 
+    end 
+
+   
+end 
+
